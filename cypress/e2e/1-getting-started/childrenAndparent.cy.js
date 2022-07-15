bbffce43c3aecdae30f1aaf4a9ea7c6596714a67
FabrizioOtranto <Fabryotranto@gmail.com>
2022-07-15 09:38:30 -0300

    Se agregarn ambientes de prueba

--- cypress/e2e/1-getting-started/childrenAndparent.cy.js
+++ cypress/e2e/1-getting-started/childrenAndparent.cy.js
@@ -3,14 +3,14 @@
 
 it("Deberia encontrar al hijo satisfactoriamente", () =>{
     cy.visit('')
-    cy.get('[role="group"]').children("input")
-    cy.get('[role="group"]').children("label")
+    cy.get('[role="group"]').children('input')
+    cy.get('[role="group"]').children('label')
 })
 
 //Para utilizar la funcion parent debemos partir de un elemento web conocido (el hijo)
 //para luego encontrar a su padre.
 it("Deberia encontrar al padre satisfactoriamente", () =>{
     cy.visit('')
-    cy.get('input#user').parent('[role="group"]')
+    cy.get("input#user").parent('[role="group"]')
 })
 
